e243b31886057f33aa28df5460fcef154325cb5d
augustomorales92 <augustomorales92@gmail.com>
2023-01-28 18:31:38 +0100

    update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,9 @@
 module.exports = {
   parserOptions: {
     parser: '@babel/eslint-parser',
-    requireConfigFile: false, // <== ADD THIS
-    ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
-    sourceType: 'module' // Allows for the use of imports
+    requireConfigFile: false,
+    ecmaVersion: 2018,
+    sourceType: 'module'
   },
     env: {
       browser: true,
