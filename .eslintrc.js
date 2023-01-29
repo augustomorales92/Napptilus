2670978d56905322257fcefc35268ba7e55e8c65
augustomorales92 <augustomorales92@gmail.com>
2023-01-29 18:40:02 +0100

    format documents

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,35 +2,31 @@ module.exports = {
   parserOptions: {
     parser: '@babel/eslint-parser',
     requireConfigFile: false,
-    ecmaVersion: "latest",
+    ecmaVersion: 'latest',
     sourceType: 'module'
   },
-    env: {
-      browser: true,
-      node: true,
-      es6: true,
-      jest: true,
-    }, 
-    extends: [
-      "eslint:recommended",
-      "plugin:react/recommended",
-      "plugin:jsx-a11y/recommended"
-    ],
-    plugins: [
-      "react",
-      "react-hooks",
-      "jsx-a11y",
-    ],
-    rules: {
-      strict: 0,
-      "react-hooks/rules-of-hooks": "error",
-      "react-hooks/exhaustive-deps": "warn",
-      "react/prop-types": 0,
-      'jsx-a11y/click-events-have-key-events': 'off'
-    },
-    settings: {
-      react: {
-        version: "detect"
-      }
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+    jest: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended'
+  ],
+  plugins: ['react', 'react-hooks', 'jsx-a11y'],
+  rules: {
+    strict: 0,
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'react/prop-types': 0,
+    'jsx-a11y/click-events-have-key-events': 'off'
+  },
+  settings: {
+    react: {
+      version: 'detect'
     }
   }
+}
